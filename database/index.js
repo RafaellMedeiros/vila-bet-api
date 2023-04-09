82d899073448248fa96618f63baa0807bd223873
Rafael Medeiros <rafaelfufcg@gmail.com>
2023-04-09 10:51:51 -0300

    adicionando script start

--- database/index.js
+++ database/index.js
@@ -1,21 +1,21 @@
 const Sequelize = require('sequelize');
 
-// const connection = new Sequelize('vilabe06_vila-bet',
-//     'vilabe06_root',
-//     'Antipam#1', 
-//     {
-//         host: 'ns1016.hostgator.com.br',
-//         dialect: "mysql"
-//     }
-// );
-
-const connection = new Sequelize('perguntas',
-    'root',
-    '663034', 
+const connection = new Sequelize(process.env.DATA_BASE_SCHEMA,
+    process.env.DATA_BASE_USER,
+    process.env.DATA_BASE_PASS, 
     {
-        host: 'localhost',
+        host: process.env.DATA_BASE_HOST,
         dialect: "mysql"
     }
 );
+
+// const connection = new Sequelize('perguntas',
+//     'root',
+//     '663034', 
+//     {
+//         host: 'localhost',
+//         dialect: "mysql"
+//     }
+// );
 
 module.exports = connection;
