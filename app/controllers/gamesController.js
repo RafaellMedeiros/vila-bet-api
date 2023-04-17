158a84d4672a559eb7669fe8d363344fe4b3e1b3
Rafael Medeiros <rafaelfufcg@gmail.com>
2023-04-17 19:36:27 -0300

    alteranbdo respostas

--- app/controllers/gamesController.js
+++ app/controllers/gamesController.js
@@ -43,7 +43,7 @@ router.post("/new-game", async (req, res) => {
     });
 
     await GameDetails.bulkCreate(resultsDetails);
-    res.status(200).send("game created");
+    res.status(200).send({msg: "game created"});
 });
 
 router.post("/", async (req, res) => {
