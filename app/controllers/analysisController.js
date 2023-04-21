dce9f94551d5ca1ce3c99cd11e6deae058205453
Rafael Medeiros <rafaelfufcg@gmail.com>
2023-04-21 10:11:51 -0300

    alterar map

--- app/controllers/analysisController.js
+++ app/controllers/analysisController.js
@@ -10,18 +10,18 @@ router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
     const { id, seller, date } = req.query;
+    const where = {};
 
-    const where = {
-        id: id ? id : null,
-        createdAt: date ? date : null
-    }
-
+    if (id) { where.id = id}
+    if (date) { where.createdAt = date}
     if (seller) {
         const user = await User.findOne({
             where: { name: seller }
         })
         where.user_id = user.cpf;
     }
+
+    console.log(where);
 
     const gameBd = await Game.findAll({
         raw: true,
@@ -43,6 +43,15 @@ router.get('/', async (req, res) => {
     }
 
     res.status(200).json(games);
+});
+
+router.get('/allUsers', async (req, res) => {
+    const allUsers = await User.findAll({
+        attributes: [ 'name', 'last_name'],
+        where: { permission: 'SELLER' }
+    });
+    const allUserFullName = allUsers.map( user => { `${user.name} ${user.last_name}`});
+    res.status(200).json(allUserFullName);
 });
 
 module.exports = app => app.use("/analysis", router);
