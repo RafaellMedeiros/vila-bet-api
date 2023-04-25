983d639379fa9f5c5972b1a5beaf14c24ea77638
Rafael Medeiros <rafaelfufcg@gmail.com>
2023-04-25 17:45:19 -0300

    Alterando validação de horario

--- app/controllers/gamesWeekController.js
+++ app/controllers/gamesWeekController.js
@@ -72,7 +72,7 @@ router.get("/", async (req, res) => {
         res.status(200).send({msg: 'games week not found', info: { allowed}});
         return;
     }
-    if (gamesWeek.length !== 0 && new Date(gamesWeek[0].limit_date).getTime() > new Date().getTime()) {
+    if (gamesWeek.length !== 0 && new Date(gamesWeek[0].limit_date).toISOString() > new Date().toISOString()) {
         allowed = true;
     }
 
