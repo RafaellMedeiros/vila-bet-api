25206b54b6cb38d8855c4685a0b860da68112df0
Rafael Medeiros <rafaelfufcg@gmail.com>
2023-04-27 13:55:33 -0300

    alterando codigo

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ require('dotenv').config()
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://vila-bet.herokuapp.com/'
-};
-app.use(cors(corsOptions)); 
+// const corsOptions = {
+//     origin: 'https://vila-bet.herokuapp.com/'
+// };
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
