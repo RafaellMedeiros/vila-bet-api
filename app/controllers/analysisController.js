eb0c7ca4ce7df30cbd5cf2d97d3d75b4ada2e2f4
Rafael Medeiros <rafaelfufcg@gmail.com>
2023-05-05 18:35:30 -0300

    alterando comportamento rota

--- app/controllers/analysisController.js
+++ app/controllers/analysisController.js
@@ -35,17 +35,19 @@ router.get('/', async (req, res) => {
     });
 
     let games = [];
-    for await (const game of gameBd) {
-        const user = await User.findOne({where: { cpf: game.user_id}});
-        const date = new Date(game.createdAt);
-        games.push({
-            id: game.id,
-            seller: `${user.name} ${user.last_name}`,
-            name: game.name,
-            telephone: game.telephone,
-            address: game.address,
-            date: ((date.getDate() )) + "/" + ((date.getMonth() + 1)) + "/" + date.getFullYear(),
-        })
+    if (gameBD.length > 0) {
+        for await (const game of gameBd) {
+            const user = await User.findOne({where: { cpf: game.user_id}});
+            const date = new Date(game.createdAt);
+            games.push({
+                id: game.id,
+                seller: `${user.name} ${user.last_name}`,
+                name: game.name,
+                telephone: game.telephone,
+                address: game.address,
+                date: ((date.getDate() )) + "/" + ((date.getMonth() + 1)) + "/" + date.getFullYear(),
+            })
+        }
     }
 
     res.status(200).json(games);
@@ -68,17 +70,19 @@ router.get('/mysales', async (req, res) => {
     });
 
     let games = [];
-    for await (const game of gameBd) {
-        const user = await User.findOne({where: { cpf: game.user_id}});
-        const date = new Date(game.createdAt);
-        games.push({
-            id: game.id,
-            seller: `${user.name} ${user.last_name}`,
-            name: game.name,
-            telephone: game.telephone,
-            address: game.address,
-            date: ((date.getDate() )) + "/" + ((date.getMonth() + 1)) + "/" + date.getFullYear(),
-        })
+    if (games.length > 0) {
+        for await (const game of gameBd) {
+            const user = await User.findOne({where: { cpf: game.user_id}});
+            const date = new Date(game.createdAt);
+            games.push({
+                id: game.id,
+                seller: `${user.name} ${user.last_name}`,
+                name: game.name,
+                telephone: game.telephone,
+                address: game.address,
+                date: ((date.getDate() )) + "/" + ((date.getMonth() + 1)) + "/" + date.getFullYear(),
+            })
+        }
     }
 
     res.status(200).json(games);
